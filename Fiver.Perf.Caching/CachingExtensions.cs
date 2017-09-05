f4f176f6499bb3aa1d9684d7dfd81fcd01ae923d
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-05 20:31:12 +0100

    asp.net core 2.0

---
+++ Fiver.Perf.Caching/CachingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Fiver.Perf.Caching
+{
+    public static class CachingExtensions
+    {
+        public static async Task SetObjectAsync<T>(
+            this IDistributedCache cache, string key, T value)
+        {
+            await cache.SetStringAsync(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static async Task<T> GetObjectAsync<T>(
+            this IDistributedCache cache, string key)
+        {
+            var value = await cache.GetStringAsync(key);
+            return value == null ? default(T) :
+                                  JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
---
+++ Fiver.Perf.Caching/UseMiddlewareExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+
+namespace Fiver.Perf.Caching
+{
+    public static class UseMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseWriteCaching(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<WriteCachingMiddleware>();
+        }
+
+        public static IApplicationBuilder UseReadCaching(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ReadCachingMiddleware>();
+        }
+    }
+
+    public class WriteCachingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IDistributedCache cache;
+
+        public WriteCachingMiddleware(
+            RequestDelegate next,
+            IDistributedCache cache)
+        {
+            this.next = next;
+            this.cache = cache;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await cache.SetObjectAsync("CurrentUser",
+                new UserInfo { Username = "James", Email = "james@bond.com" });
+            await this.next(context);
+        }
+    }
+
+    public class ReadCachingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IDistributedCache cache;
+
+        public ReadCachingMiddleware(
+            RequestDelegate next,
+            IDistributedCache cache)
+        {
+            this.next = next;
+            this.cache = cache;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var user = await cache.GetObjectAsync<UserInfo>("CurrentUser");
+            await context.Response.WriteAsync($"{user.Username}, {user.Email}");
+        }
+    }
+}
